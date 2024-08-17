5a64e8ee6575e12f41685c7c5ef7626f82a297fe
David Rabko <david.rabko@icloud.com>
2024-08-17 14:07:28 +0200

    Add first two get-started steps

---
+++ 00-helloworld/src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+package org.studies
+
+fun main() {
+    val name = "Mary"
+    val age = 20
+    println("$name is $age years old")
+}
---
+++ 01-basictypes/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+package org.studies
+
+fun main() {
+    val a: Int = 1000
+    val b: String = "log message"
+    val c: Double = 3.14
+    val d: Long = 100_000_000_000_000
+    val e: Boolean = false
+    val f: Char = '\n'
+}
