b2102eb710f292053d2de45d8428623617fb2c7d
David Rabko <david.rabko@icloud.com>
2024-08-21 10:26:55 +0200

    Add lambdas

---
+++ 04-functions/src/main/kotlin/lambdas/Exercise1.kt
@@ -0,0 +1,9 @@
+package lambdas
+
+fun main() {
+    val actions = listOf("title", "year", "author")
+    val prefix = "https://example.com/book-info"
+    val id = 5
+    val urls = actions.map { action -> "$prefix/$id/$action" }
+    println(urls)
+}
