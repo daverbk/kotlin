9aa76b5d6277a6be4b0b6d3a36d188546f30976d
David Rabko <davidryabko13@gmail.com>
2024-08-26 09:49:55 +0200

    Add data class

--- spring/src/main/kotlin/org/studies/spring/Application.kt
+++ spring/src/main/kotlin/org/studies/spring/Application.kt
@@ -3,7 +3,6 @@ package org.studies.spring
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
@@ -13,8 +12,14 @@ fun main(args: Array<String>) {
     runApplication<Application>(*args)
 }
 
-@RestController("/")
+@RestController
 class MessageController {
-    @GetMapping
-    fun index(@RequestParam("name") name: String) = "Hello, $name!"
+    @GetMapping("/")
+    fun index() = listOf(
+        Message("1", "Hello!"),
+        Message("2", "Bonjour!"),
+        Message("3", "Privet!"),
+    )
 }
+
+data class Message(val id: String?, val text: String)
