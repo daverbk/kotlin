4e0cb8475feaac06732e76da36f32e10ff548c52
David <davidryabko13@gmail.com>
2024-09-08 18:37:20 +0300

    Add kotlin koans: classes

--- spring/build.gradle.kts
+++ spring/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
+	kotlin("jvm") version "2.0.0"
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.3.3"
 	id("io.spring.dependency-management") version "1.1.6"
