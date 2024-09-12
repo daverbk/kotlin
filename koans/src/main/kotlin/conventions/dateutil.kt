8549607ecbce528e6113917b4457bc4637dfd6ae
David Rabko <david.rabko@icloud.com>
2024-09-12 23:45:37 +0300

    Finish operator overloading

---
+++ koans/src/main/kotlin/conventions/dateutil.kt
@@ -0,0 +1,29 @@
+package conventions
+
+import java.util.*
+
+fun MyDate.followingDate(): MyDate {
+    val c = Calendar.getInstance()
+    c.set(year, month, dayOfMonth)
+    val milliseconds = 24 * 60 * 60 * 1000
+    val timeInMillis = c.timeInMillis + milliseconds
+    val result = Calendar.getInstance()
+    result.timeInMillis = timeInMillis
+    return MyDate(result.get(Calendar.YEAR), result.get(Calendar.MONTH), result.get(Calendar.DATE))
+}
+
+fun MyDate.addTimeIntervals(timeInterval: TimeInterval, amount: Int): MyDate {
+    val c = Calendar.getInstance()
+    c.set(year + if (timeInterval == TimeInterval.YEAR) amount else 0, month, dayOfMonth)
+    var timeInMillis = c.timeInMillis
+    val millisecondsInADay = 24 * 60 * 60 * 1000L
+    timeInMillis += amount * when (timeInterval) {
+        TimeInterval.DAY -> millisecondsInADay
+        TimeInterval.WEEK -> 7 * millisecondsInADay
+        TimeInterval.YEAR -> 0L
+    }
+
+    var result = Calendar.getInstance()
+    result.timeInMillis = timeInMillis
+    return MyDate(result.get(Calendar.YEAR), result.get(Calendar.MONTH), result.get(Calendar.DATE))
+}
