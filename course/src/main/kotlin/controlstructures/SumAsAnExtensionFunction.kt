45126d651f6966d7a7b075742655536deaef7161
David <davidryabko13@gmail.com>
2024-10-16 00:39:26 +0300

    Complete the second course task

---
+++ course/src/main/kotlin/controlstructures/SumAsAnExtensionFunction.kt
@@ -0,0 +1,14 @@
+package controlstructures
+
+fun sum(list: List<Int>): Int {
+    var result = 0
+    for (i in list) {
+        result += i
+    }
+    return result
+}
+
+fun main(args: Array<String>) {
+    val sum = sum(listOf(1, 2, 3))
+    println(sum)
+}
