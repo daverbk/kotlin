28a129b98cd7f521fa7a87648babd8b6477b1bb2
David <davidryabko13@gmail.com>
2024-11-10 14:14:29 +0300

    Add the third taxi park task

--- course/src/main/kotlin/taxipark/TaxiParkTask.kt
+++ course/src/main/kotlin/taxipark/TaxiParkTask.kt
@@ -16,3 +16,11 @@ fun TaxiPark.findFaithfulPassengers(minTrips: Int): Set<Passenger> =
     allPassengers.filter { passenger ->
         trips.count { it.passengers.contains(passenger) } >= minTrips
     }.toSet()
+
+/*
+ * Task #3. Find all the passengers, who were taken by a given driver more than once.
+ */
+fun TaxiPark.findFrequentPassengers(driver: Driver): Set<Passenger> =
+    allPassengers.filter { passenger ->
+        trips.count { it.driver == driver && it.passengers.contains(passenger) } > 1
+    }.toSet()
