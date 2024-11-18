3be125e10f86d38c2e115ed46b9d867ec37ed3f6
David <davidryabko13@gmail.com>
2024-11-18 23:04:36 +0300

    Finish conventions

---
+++ course/src/main/kotlin/conventions/Equality.kt
@@ -0,0 +1,27 @@
+package conventions
+
+data class Value(val s: String)
+
+fun equals1(v1: Value?, v2: Value?): Boolean {
+    return v1 == v2
+}
+
+fun equals2(v1: Value?, v2: Value?): Boolean = v1?.equals(v2) ?: (v1 === v2)
+
+fun main() {
+    equals1(Value("abc"), Value("abc")) eq true
+    equals1(Value("abc"), null) eq false
+    equals1(null, Value("abc")) eq false
+    equals1(null, null) eq true
+
+    equals2(Value("abc"), Value("abc")) eq true
+    equals2(Value("abc"), null) eq false
+    equals2(null, Value("abc")) eq false
+    equals2(null, null) eq true
+}
+
+private infix fun Boolean.eq(expected: Boolean) {
+    if (this == expected) println("OK")
+    else println("FAIL")
+}
+
