3da1f2afb2a8b0d23dc55015069fc215ae535c49
David Rabko <david.rabko@icloud.com>
2024-12-13 23:49:58 +0300

    Start chapter 5

---
+++ course/src/main/kotlin/filterinlining/FilterInlining.kt
@@ -0,0 +1,33 @@
+package filterinlining
+
+fun filterNonZero(list: List<Int>) = list.filter { it != 0 }
+
+fun filterNonZeroGenerated(list: List<Int>): List<Int> {
+    val destination = ArrayList<Int>()
+    for (element in list) {
+        if (element != 0) {
+            destination.add(element)
+        }
+    }
+    return destination
+}
+
+fun main() {
+    val list = listOf(1, 2, 3)
+
+    filterNonZero(list).toString() eq "[1, 2, 3]"
+    filterNonZeroGenerated(list).toString() eq "[1, 2, 3]"
+}
+
+private infix fun String.eq(s: String) = if (this == s) println("OK") else println("NOT OK")
+
+inline fun <T> Iterable<T>.filter(predicate: (T) -> Boolean): List<T> {
+    val destination = ArrayList<T>()
+    for (element in this) {
+        if (predicate(element)) {
+            destination.add(element)
+        }
+    }
+    return destination
+}
+
