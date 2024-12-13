79509888239c967eab77f8a4995f9393f8d7e858
David Rabko <david.rabko@icloud.com>
2024-12-14 01:15:20 +0300

    Finish sequences

---
+++ course/src/main/kotlin/fibonacci/FibonacciSequence.kt
@@ -0,0 +1,20 @@
+package fibonacci
+
+import filterinlining.eq
+
+fun fibonacci(): Sequence<Int> = sequence {
+    var elements = Pair(0, 1)
+
+    while (true) {
+        yield(elements.first)
+        elements = Pair(elements.second, elements.first + elements.second)
+    }
+}
+
+fun main() {
+    fibonacci().take(4).toList().toString() eq
+            "[0, 1, 1, 2]"
+
+    fibonacci().take(10).toList().toString() eq
+            "[0, 1, 1, 2, 3, 5, 8, 13, 21, 34]"
+}
